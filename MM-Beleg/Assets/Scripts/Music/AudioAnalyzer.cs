b37031cf98435430de9303712690da9953b79058
Zombatar33 <ageeadebied@gmail.com>
2022-07-12 22:48:23 +0200

    FX, Musik, Sprites, Shop, Main Menu
    -

---
+++ MM-Beleg/Assets/Scripts/Music/AudioAnalyzer.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioAnalyzer : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public float updateStep = 0.1f;
+    public int sampleDataLength = 1024;
+
+    private float currentUpdateTime = 0f;
+
+    private float clipLoudness;
+    private float[] clipSampleData;
+
+    private float fMax;
+
+    void Awake()
+    {
+
+        if (!audioSource)
+        {
+            Debug.LogError(GetType() + ".Awake: there was no audioSource set.");
+        }
+        clipSampleData = new float[sampleDataLength];
+        fMax = AudioSettings.outputSampleRate / 2;
+
+    }
+
+    void Update()
+    {
+
+        currentUpdateTime += Time.deltaTime;
+        if (currentUpdateTime >= updateStep)
+        {
+            currentUpdateTime = 0f;
+            audioSource.clip.GetData(clipSampleData, audioSource.timeSamples); //I read 1024 samples, which is about 80 ms on a 44khz stereo clip, beginning at the current sample position of the clip.
+            clipLoudness = 0f;
+            // Average
+            foreach (var sample in clipSampleData)
+            {
+                clipLoudness += Mathf.Abs(sample);
+            }
+            clipLoudness /= sampleDataLength; //clipLoudness is what you are looking for
+
+        }
+
+    }
+
+    public float GetLoudness()
+    {
+        return clipLoudness;
+    }
+}
